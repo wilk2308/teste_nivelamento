159b2f0f3acf47f35cec76bf723a32c7a1ba094e
Will Sousa <williamdf2308@gmail.com>
2025-03-27 22:10:46 -0300

    Update

--- sql/import_data.sql
+++ sql/import_data.sql
@@ -1,19 +1,18 @@
 -- Importar os dados do CSV para a tabela operadoras
-LOAD DATA INFILE '/dados/Relatorio_cadop.csv'
-INTO TABLE operadoras
-FIELDS TERMINATED BY ';'
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(Registro_ANS, CNPJ, Razao_Social, Nome_Fantasia, Modalidade, Logradouro, Numero, 
- Complemento, Bairro, Cidade, UF, CEP, DDD, Telefone, Fax, Endereco_eletronico, 
- Representante, Cargo_Representante, Regiao_de_Comercializacao, Data_Registro_ANS);
+LOAD DATA INFILE 'C:/Users/Administrador/Documents/GitHub/teste_nivelamento/dados/Relatorio_cadop.csv'
+INTO TABLE operadoras 
+FIELDS TERMINATED BY ';'  
+ENCLOSED BY '"' 
+IGNORE 1 LINES 
+(Registro_ANS, CNPJ, Razao_Social, Nome_Fantasia, Modalidade, Logradouro, Numero,  
+Complemento, Bairro, Cidade, UF, CEP, DDD, Telefone, Fax, Endereco_eletronico,  
+Representante, Cargo_Representante, Regiao_de_Comercializacao, Data_Registro_ANS);
+
 
 -- Importar os dados do CSV para a tabela demonstracoes_contabeis
-LOAD DATA INFILE '/dados/demonstracoes_contabeis.csv'
+LOAD DATA INFILE 'C:/Users/Administrador/Documents/GitHub/teste_nivelamento/dados/demonstracoes_contabeis.csv'
 INTO TABLE demonstracoes_contabeis
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (DATA, REG_ANS, CD_CONTA_CONTABIL, DESCRICAO, VL_SALDO_INICIAL, VL_SALDO_FINAL);
